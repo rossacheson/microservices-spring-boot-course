e7a25abbabed20f8bd00a734575512c934692ac1
Ross Acheson <rossacheson@users.noreply.github.com>
2025-02-14 18:02:22 -0600

    Add circuit breaker to accounts routes

--- section-10/gatewayserver/src/main/java/com/rossacheson/gatewayserver/GatewayserverApplication.java
+++ section-10/gatewayserver/src/main/java/com/rossacheson/gatewayserver/GatewayserverApplication.java
@@ -21,7 +21,9 @@ public class GatewayserverApplication {
 				.route(p -> p
 						.path("/eazybank/accounts/**")
 						.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.circuitBreaker(config -> config.setName("accountsCircuitBreaker"))
+						)
 						.uri("lb://ACCOUNTS")) // direct to appropriate load balancer
 				.route(p -> p
 						.path("/eazybank/loans/**")
